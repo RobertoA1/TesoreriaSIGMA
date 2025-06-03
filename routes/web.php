be2f18a0a835583fb63aac15a313b53643186212
Adrian Gomez <afgomezro@unitru.edu.pe>
2025-06-03 12:37:09 -0500

    Primer avance de alumnos, hasta tablas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\NivelEducativoController;
 use App\Http\Controllers\FamiliarController;
+use App\Http\Controllers\AlumnoController;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,18 @@ Route::group(['middleware' => ['auth', 'can:access-resource,"academica"']], func
     Route::patch('/niveles-academicos/{id}/editar', [NivelEducativoController::class, 'editEntry']);
 
     Route::delete('/niveles-academicos/', [NivelEducativoController::class, 'delete']);
+});
+
+Route::group(['middleware' => ['auth', 'can:access-resource,"alumnos"']], function(){
+    Route::get('/alumnos', [AlumnoController::class, 'index'])->name('alumno_view');
+
+    Route::get('/alumnos/crear', [AlumnoController::class, 'create'])->name('alumno_create');
+    Route::put('/alumnos/crear', [AlumnoController::class, 'createNewEntry'])->name('alumno');
+
+    Route::get('/alumnos/{id}/editar', [AlumnoController::class, 'edit'])->name('alumno_edit');
+    Route::patch('/alumnos/{id}/editar', [AlumnoController::class, 'editEntry']);
+
+    Route::delete('/alumnos/', [AlumnoController::class, 'delete']);
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
